c014a43186aee7454d22f47b6d1b11d4e5464626
unknown <satria.munthe513@gmail.com>
2024-04-19 10:07:48 +0700

    regex fix(2)

--- regular-expression/script.js
+++ regular-expression/script.js
@@ -1,9 +1,9 @@
-let regexInputsat = document.querySelector("#regexInputsat");
+let regexInputSat = document.querySelector("#regexInputSat");
 let validationTextsat = document.querySelector("#validation-text-sat");
 
 const REGULAR_EXPRESSION = /^[^\d]{10,}$/;
 
-regexInputsat.addEventListener("input", function() {
-    validationTextsat.innerHTML = REGULAR_EXPRESSION.test(regexInputsat.value) 
-    ? "<p class='text-green-600'>Valid</p>" : "<p class='text-red-600'>Invalid</p>";
+regexInputSat.addEventListener("input", function() {
+    validationTextsat.textContent = REGULAR_EXPRESSION.test(regexInputSat.value) 
+    ? "Valid" : "Invalid, minimum 10 characters required.";
 });
