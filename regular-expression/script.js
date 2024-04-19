9c6822d9724027ee170e5cf5ac2067d95808e2ec
unknown <satria.munthe513@gmail.com>
2024-04-19 09:52:52 +0700

    regex fix

--- regular-expression/script.js
+++ regular-expression/script.js
@@ -1,9 +1,9 @@
-let regexInputAndra = document.querySelector("#regexInputAndra");
-let validationTextAndra = document.querySelector("#validation-text-andra");
+let regexInputsat = document.querySelector("#regexInputsat");
+let validationTextsat = document.querySelector("#validation-text-sat");
 
 const REGULAR_EXPRESSION = /^[^\d]{10,}$/;
 
-regexInputAndra.addEventListener("input", function() {
-    validationTextAndra.innerHTML = REGULAR_EXPRESSION.test(regexInputAndra.value) 
+regexInputsat.addEventListener("input", function() {
+    validationTextsat.innerHTML = REGULAR_EXPRESSION.test(regexInputsat.value) 
     ? "<p class='text-green-600'>Valid</p>" : "<p class='text-red-600'>Invalid</p>";
 });
